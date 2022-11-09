c4ebadcd41aa0042c6133991556f85f06cb88309
Gabriel Patrocinio <gabriel060120@gmail.com>
2022-11-09 19:07:26 -0300

    feat: dashboard impl

--- lib/features/dashboard/pages/dashboard_page.dart
+++ lib/features/dashboard/pages/dashboard_page.dart
@@ -1,4 +1,6 @@
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:charity_finder/features/dashboard/components/carousel_widget.dart';
+import 'package:charity_finder/features/dashboard/components/product_dashboard_widget.dart';
 import 'package:charity_finder/features/onboarding/ui/components/carousel.dart';
 import 'package:charity_finder/features/onboarding/ui/components/carousel_page.dart';
 import 'package:flutter/material.dart';
@@ -20,7 +22,10 @@ class _DashboardPageState extends State<DashboardPage> {
     return Scaffold(
       drawer: const Drawer(),
       appBar: AppBar(
-        title: const Text('CharityFinder'),
+        title: const Text(
+          'CharityFinder',
+          style: TextStyle(fontSize: 22),
+        ),
         actions: [
           IconButton(
             onPressed: () {},
@@ -31,40 +36,131 @@ class _DashboardPageState extends State<DashboardPage> {
           ),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(
-              height: 10,
-            ),
-            const Text(
-              'Destaques (Instituições)',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
-            ),
-            const SizedBox(
-              height: 16,
-            ),
-            CarouselSlider(
-              options: CarouselOptions(
-                height: 120,
-                enableInfiniteScroll: false,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(
+                height: 10,
               ),
-              items: [1, 2, 3, 4, 5].map((i) {
-                return Builder(
-                  builder: (BuildContext context) {
-                    return Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                      child: Placeholder(
-                        fallbackWidth: MediaQuery.of(context).size.width * .8,
-                      ),
-                    );
-                  },
-                );
-              }).toList(),
-            )
-          ],
+              const Text(
+                'Destaques (Instituições)',
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.w500),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              const CarouselWidget(),
+              const SizedBox(
+                height: 18,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'Instituições',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      print("Ver mais pressionado");
+                    },
+                    child: const Text(
+                      "Ver mais >",
+                      style: TextStyle(fontSize: 14),
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                height: 150,
+                width: MediaQuery.of(context).size.width,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    ProductDashboardWidget(
+                      text: "Lorem ipsum dolor sit amet",
+                      onTap: () {},
+                    ),
+                    ProductDashboardWidget(
+                      text: "Lorem ipsum dolor sit amet",
+                      onTap: () {},
+                    ),
+                    ProductDashboardWidget(
+                      text: "Lorem ipsum dolor sit amet",
+                      onTap: () {},
+                    ),
+                    ProductDashboardWidget(
+                      text: "Lorem ipsum dolor sit amet",
+                      onTap: () {},
+                    ),
+                    ProductDashboardWidget(
+                      text: "Lorem ipsum dolor sit amet",
+                      onTap: () {},
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'Doações Necessárias',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      print("Ver mais pressionado");
+                    },
+                    child: const Text(
+                      "Ver mais >",
+                      style: TextStyle(fontSize: 14),
+                    ),
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                height: 150,
+                width: MediaQuery.of(context).size.width,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    ProductDashboardWidget(
+                      text: "Lorem ipsum dolor sit amet",
+                      onTap: () {},
+                    ),
+                    ProductDashboardWidget(
+                      text: "Lorem ipsum dolor sit amet",
+                      onTap: () {},
+                    ),
+                    ProductDashboardWidget(
+                      text: "Lorem ipsum dolor sit amet",
+                      onTap: () {},
+                    ),
+                    ProductDashboardWidget(
+                      text: "Lorem ipsum dolor sit amet",
+                      onTap: () {},
+                    ),
+                    ProductDashboardWidget(
+                      text: "Lorem ipsum dolor sit amet",
+                      onTap: () {},
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
